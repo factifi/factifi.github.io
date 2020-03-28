032416838cd214037243363efbae0af99cb354b4
Pieter-Jan <pj.beeckman@gmail.com>
2020-03-28 15:53:56 +0100

    add label

--- content.js
+++ content.js
@@ -8,8 +8,30 @@ function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+function get_label_html(label) {
+  return (
+    '<span class="fact-check-label label-onwaar" style="' +
+    "position: absolute;" +
+    "display: block;" +
+    "top: 0px;" +
+    "right: 0px;" +
+    "background-color: red;" +
+    "/* z-index: 999999999; */" +
+    "font-size: 30px;" +
+    "color: white;" +
+    "/* width: 200px; */" +
+    "/* height: 100px; */" +
+    "line-height: 30px;" +
+    "padding: 10px;" +
+    '">' +
+    label +
+    "</span>"
+  );
+}
+
 function init() {
-  console.log("init Hack the Crisis", article_html);
+  console.log("init Hack the Crisis", chrome.runtime);
+  //chrome.browserAction.setBadgeText({ text: "0" }); // We have 10+ unread items.
 
   var tablink = window.location.href;
 
@@ -27,11 +49,12 @@ function init() {
       if (post.hasClass("check-post-attr") || found.indexOf(txt) > -1) {
         // console.log("Already processed", post.data("check-id"), txt);
       } else {
-        console.log("NEW!");
+        console.log("NEW!", "visible?:", $(post).is(":visible"));
         post.addClass("check-post-attr");
         post.data("check-id", i);
         found.push(txt);
         console.log("Header found:", i, txt);
+        //chrome.browserAction.setBadgeText({ text: found.length + "" }); // We have 10+ unread items.
 
         var requestOptions = {
           method: "POST",
@@ -41,8 +64,16 @@ function init() {
         };
 
         fetch("https://api-v2.factrank.org/match", requestOptions)
-          .then(response => response.text())
-          .then(result => console.log("Res", result, post))
+          .then(response => response.json())
+          .then(result => {
+            console.log("Res", result[0], post);
+            const img_container = $(post)
+              .find("img.scaledImageFitWidth.img")
+              .parent("div");
+
+            $(img_container).append(get_label_html(result[0].conclusion));
+            //console.log($(post).find("img.scaledImageFitWidth.img").length);
+          })
           .catch(error => console.log("error", error));
       }
 
@@ -52,9 +83,11 @@ function init() {
 }
 init();
 
+/*
 // Inform the background page that
 // this tab should have a page-action
 chrome.runtime.sendMessage({
   from: "content",
   subject: "showPageAction"
 });
+*/
