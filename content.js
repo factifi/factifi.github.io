99750e62c9ef0c0c34a59f8482fc4d5aac8a1c9c
Pieter-Jan <pj.beeckman@gmail.com>
2020-03-29 11:43:33 +0200

    mooi ui

--- content.js
+++ content.js
@@ -1,29 +1,52 @@
 // content.js
-
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Content-Type", "text/plain");
-
 function insertAfter(referenceNode, newNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
-
 function get_label_html(label, src) {
-  let color = "red";
+  let text = "";
+  let border = "";
+  let icon = "";
+
   if (label.toUpperCase() == "ONDUIDELIJK") {
-    color = "orange";
+    icon = onzeker_icon;
+    border = "orange";
+    text = "Verifi.fi heeft geen info" + "<br>" + "gevonden voor dit artikel.";
   }
   if (label.toUpperCase() == "NO MATCH") {
-    color = "grey";
+    icon = nomatch_icon;
+    border = "yellow";
+    text =
+      "Verifi.fi heeft geen overeeenkomsten" +
+      "<br>" +
+      "gevonden voor dit artikel.            ";
   }
-
+  if (label.toUpperCase() == "ONWAAR") {
+    icon = onwaar_icon;
+    border = "red";
+    text =
+      // '<img src="web-icons/onwaar.svg>' +
+      "Verifi.fi is <strong style='color: black;'>80% zeker</strong> dat dit artikel" +
+      "<br>" +
+      "onwaarheden bevat.                 ";
+  }
+  if (label.toUpperCase() == "WAAR") {
+    icon = waar_icon;
+    border = "green";
+    text =
+      "Verifi.fi is <strong style='color: black;'>100% zeker</strong> dat dit artikel" +
+      "<br>" +
+      "verified is.";
+  }
   if (src == "hln")
     return (
       '<span class="fact-check-label label-onwaar" style="' +
       "position: absolute;" +
       "display: block;" +
       "top: 10px;" +
-      "left: -20px;" +
+      "left: 20px" +
       "background-color: " +
       color +
       ";" +
@@ -41,34 +64,30 @@ function get_label_html(label, src) {
   else
     return (
       '<span class="fact-check-label label-onwaar" style="' +
-      "position: absolute;" +
-      "display: block;" +
-      "top: 150px;" +
-      "left: -20px;" +
-      "background-color: " +
-      color +
+      "border: 1px solid " +
+      border +
       ";" +
-      "z-index: 999999999;" +
-      "font-size: 30px;" +
-      "color: white;" +
-      "/* width: 200px; */" +
-      "/* height: 100px; */" +
-      "line-height: 30px;" +
-      "padding: 10px;" +
       '">' +
+      icon +
+      '<span class="hover-content">' +
+      "<strong style='color: black;'>" +
       label +
+      "</strong>" +
+      "<span class='info'>" +
+      "  " +
+      text +
+      "</span>" +
+      "<a href='#'>Meer info..</a>" +
+      "<span class='arrow'>></span>" +
+      "</span>" +
       "</span>"
     );
 }
-
 function init() {
   console.log("init Hack the Crisis", chrome.runtime);
   //chrome.browserAction.setBadgeText({ text: "0" }); // We have 10+ unread items.
-
   var tablink = window.location.href;
-
   console.log("tablink is:", tablink);
-
   switch (tablink) {
     case "https://www.facebook.com/":
       run_fb();
@@ -82,13 +101,10 @@ function init() {
   }
 }
 init();
-
 function run_hln() {
   let arr_articles = [];
   let arr_txts = [];
-
   let s_i = 0;
-
   $(".page-section").each(function() {
     s_i++;
     console.log("Section", s_i);
@@ -104,7 +120,6 @@ function run_hln() {
               .trim();
             //console.log(txt.length, txt);
           });
-
         if ($(this).find("picture").length && $(this).find("h1").length) {
           const post = this;
           const txt = $(this)
@@ -112,29 +127,24 @@ function run_hln() {
             .first()
             .text()
             .trim();
-
           var requestOptions = {
             method: "POST",
             headers: myHeaders,
             body: JSON.stringify([txt]),
             redirect: "follow"
           };
-
-          fetch("https://api-v2.factrank.org/match", requestOptions)
+          fetch("https://api-v2.factrank.org/bert-match", requestOptions)
             .then(response => response.json())
             .then(result => {
               console.log("Res", result);
-
               //for (var a = 0; a < result.length; a++) {
               //const _txt = arr_txts[a];
               //const _post = $(arr_articles[a]);
-
               if (
                 result[0].hasOwnProperty("matched") &&
                 result[0].matched === false
               ) {
                 console.log("No match", txt);
-
                 $(post)
                   .css("position", "relative")
                   .append(get_label_html("NO MATCH", "hln"));
@@ -145,14 +155,11 @@ function run_hln() {
                   .append(get_label_html(result[0].conclusion, "hln"));
               }
               //}
-
               //console.log($(post).find("img.scaledImageFitWidth.img").length);
             })
             .catch(error => console.log("error", error));
-
           //arr_txts.push(txt);
           //arr_articles.push(post);
-
           //console.log("valid");
         } else {
           console.log(
@@ -161,7 +168,6 @@ function run_hln() {
             $(this).find("h1").length
           );
         }
-
         /*
       $(this)
         .find("h1")
@@ -181,22 +187,18 @@ function run_hln() {
     body: JSON.stringify(arr_txts),
     redirect: "follow"
   };
-
-  fetch("https://api-v2.factrank.org/match", requestOptions)
+  fetch("https://api-v2.factrank.org/bert-match", requestOptions)
     .then(response => response.json())
     .then(result => {
       console.log("Res", result);
-
       for (var a = 0; a < result.length; a++) {
         const _txt = arr_txts[a];
         const _post = $(arr_articles[a]);
-
         if (
           result[a].hasOwnProperty("matched") &&
           result[a].matched === false
         ) {
           console.log("No match", _txt);
-
           $(_post)
             .css("position", "relative")
             .append(get_label_html("NO MATCH " + a, "hln"));
@@ -207,26 +209,29 @@ function run_hln() {
             .append(get_label_html(result[a].conclusion + " " + a, "hln"));
         }
       }
-
       //console.log($(post).find("img.scaledImageFitWidth.img").length);
     })
     .catch(error => console.log("error", error));
     */
 }
-
 function run_fb() {
   var demo_post = document.createElement("div");
   demo_post.innerHTML = article_html;
   insertAfter(document.getElementById("substream_0"), demo_post);
-
   let i = 0;
   let found = [];
   setInterval(function() {
     $('a[aria-label][target="_blank"]').each(function() {
       var post = $(this).closest("[data-dedupekey]");
-      const txt = $(this)
+      let txt = $(this)
         .attr("aria-label")
         .trim();
+
+      if (!txt.endsWith(".")) {
+        txt += ".";
+      }
+
+      txt = txt.replace(/^\w/, c => c.toUpperCase());
 
       if (
         !$(post).is(":visible") ||
@@ -241,23 +246,19 @@ function run_fb() {
         found.push(txt);
         console.log("Header found:", i, txt);
         //chrome.browserAction.setBadgeText({ text: found.length + "" }); // We have 10+ unread items.
-
         var requestOptions = {
           method: "POST",
           headers: myHeaders,
           body: JSON.stringify([txt]),
           redirect: "follow"
         };
-
-        fetch("https://api-v2.factrank.org/match", requestOptions)
+        fetch("https://api-v2.factrank.org/bert-match", requestOptions)
           .then(response => response.json())
           .then(result => {
             console.log("Res", result, txt);
-
             const img_container = $(post)
               .find("img.scaledImageFitWidth.img")
               .parent("div");
-
             if (
               result[0].hasOwnProperty("matched") &&
               result[0].matched === false
@@ -267,17 +268,14 @@ function run_fb() {
             } else {
               $(post).append(get_label_html(result[0].conclusion, "fb"));
             }
-
             //console.log($(post).find("img.scaledImageFitWidth.img").length);
           })
           .catch(error => console.log("error", error));
       }
-
       i++;
     });
   }, 500);
 }
-
 /*
 // Inform the background page that
 // this tab should have a page-action
